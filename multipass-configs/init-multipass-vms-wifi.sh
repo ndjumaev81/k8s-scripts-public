e3f253a483ff087f007d8289a299528e2c5b3a5c
nodirdjumaev <nodir.djumaev@gmail.com>
2025-04-05 23:36:01 +0500

    standalone script to run multipass multiple vms

--- multipass-configs/init-multipass-vms-wifi.sh
+++ multipass-configs/init-multipass-vms-wifi.sh
@@ -4,18 +4,6 @@
 CURRENT_USER=$(whoami)
 echo "Current user: $CURRENT_USER"
 
-# Launch VMs with specified parameters
-#./launch-vm-1.sh 2 4 20 master &
-#./launch-vm-1.sh 4 6 20 worker-1 &
-#./launch-vm-1.sh 4 6 20 worker-2 &
-#./launch-vm-1.sh 4 6 20 worker-3 &
-#./launch-vm-1.sh 4 6 20 worker-4 &
-
-# echo "multipass vms launched..."
-
-# Wait for all background VM launches to complete
-#wait
-
 # Install dnsmasq if not installed
 if ! command -v dnsmasq &> /dev/null; then
     brew install dnsmasq
