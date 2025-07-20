78074ce6ae1e39798f039ad53a914de1499c7588
nodirdjumaev <nodir.djumaev@gmail.com>
2025-07-20 22:22:40 +0500

    fix deprecation warning

--- asterisk/transcribe.sh
+++ asterisk/transcribe.sh
@@ -2,7 +2,7 @@
 SOURCE="/tmp/recording.wav"
 FORMATTED="/tmp/recording16.wav"
 MODEL="$HOME/whisper.cpp/models/ggml-medium.bin"
-BIN="/usr/local/bin/whisper"
+BIN="/usr/local/bin/whisper-cli"
 
 ffmpeg -y -i "$SOURCE" -ar 16000 -ac 1 -c:a pcm_s16le "$FORMATTED"
 
